65e01e75c4aede58cbe180f20a6abee4eecf4b98
kobewi <kobewi4e@gmail.com>
2024-04-03 11:54:07 +0200

    The plugin

---
+++ addons/parallax2d_preview/Parallax2DPreview.gd
@@ -0,0 +1,86 @@
+@tool
+extends EditorPlugin
+
+enum { PREVIEW_DISABLED, PREVIEW_ACCURATE, PREVIEW_BASIC, REFRESH_PREVIEW }
+
+var preview_button: MenuButton
+var button_popup: Popup
+var preview_node: Node
+
+func _enter_tree() -> void:
+	preview_button = MenuButton.new()
+	preview_button.text = "Parallax2D Preview"
+	preview_button.hide()
+	preview_button.theme_changed.connect(update_preview_button_theme)
+	add_control_to_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, preview_button)
+	
+	var menu := preview_button.get_popup()
+	menu.id_pressed.connect(on_menu_selected)
+	
+	menu.add_radio_check_item("Preview Disabled", PREVIEW_DISABLED)
+	menu.add_radio_check_item("Accurate Preview", PREVIEW_ACCURATE)
+	menu.add_radio_check_item("Basic Preview", PREVIEW_BASIC)
+	menu.add_separator()
+	menu.add_item("Refresh Preview", REFRESH_PREVIEW)
+	on_menu_selected(PREVIEW_DISABLED)
+	
+	scene_changed.connect(func(s): on_menu_selected(PREVIEW_DISABLED))
+	EditorInterface.get_selection().selection_changed.connect(update_button)
+
+func _exit_tree() -> void:
+	remove_control_from_container(EditorPlugin.CONTAINER_CANVAS_EDITOR_MENU, preview_button)
+	preview_button.queue_free()
+
+func update_preview_button_theme():
+	preview_button.icon = preview_button.get_theme_icon(&"Parallax2D", &"EditorIcons")
+
+func update_button():
+	var make_visible: bool
+	
+	var selection := EditorInterface.get_selection().get_selected_nodes()
+	if selection.size() == 1:
+		make_visible = selection[0] is CanvasItem
+	
+	preview_button.visible = make_visible
+
+func on_menu_selected(id: int):
+	match id:
+		PREVIEW_DISABLED:
+			if preview_node:
+				preview_node.queue_free()
+				preview_node = null
+		
+		PREVIEW_ACCURATE:
+			if preview_node:
+				preview_node.accurate_preview = true
+			else:
+				make_node(true)
+		
+		PREVIEW_BASIC:
+			if preview_node:
+				preview_node.accurate_preview = false
+			else:
+				make_node(false)
+		
+		REFRESH_PREVIEW:
+			if not preview_node:
+				return
+			
+			var accurate: bool = preview_node.accurate_preview
+			preview_node.queue_free()
+			make_node(accurate)
+	
+	var menu := preview_button.get_popup()
+	if id < REFRESH_PREVIEW:
+		for i in REFRESH_PREVIEW:
+			menu.set_item_checked(i, id == i)
+	
+	menu.set_item_disabled(menu.get_item_index(REFRESH_PREVIEW), preview_node == null)
+
+func make_node(accurate_preview: bool):
+	if not EditorInterface.get_edited_scene_root():
+		return
+	
+	preview_node = preload("res://addons/parallax2d_preview/PreviewNode.gd").new()
+	preview_node.accurate_preview = accurate_preview
+	EditorInterface.get_edited_scene_root().add_child(preview_node)
---
+++ addons/parallax2d_preview/PreviewNode.gd
@@ -0,0 +1,98 @@
+@tool
+extends Node
+
+var parallax_list: Array[Parallax2D]
+var accurate_preview: bool:
+	set = update_preview
+var window_size: Vector2
+
+var last_viewport_pos: Vector2 = Vector2.INF
+var line_helper: Node2D
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_ENTER_TREE:
+			if parallax_list.is_empty():
+				find_parallax(get_tree().edited_scene_root)
+				window_size = Vector2(
+					ProjectSettings.get_setting_with_override(&"display/window/size/viewport_width"),
+					ProjectSettings.get_setting_with_override(&"display/window/size/viewport_height")
+				)
+			
+			update_preview()
+		
+		NOTIFICATION_EXIT_TREE:
+			for parallax in parallax_list:
+				parallax.screen_offset = Vector2()
+			
+			if line_helper:
+				line_helper.queue_free()
+		
+		NOTIFICATION_EDITOR_PRE_SAVE:
+			for parallax in parallax_list:
+				parallax.screen_offset = Vector2()
+		
+		NOTIFICATION_EDITOR_POST_SAVE:
+			last_viewport_pos = Vector2.INF
+
+func find_parallax(parent: Node):
+	for child in parent.get_children():
+		if child is Parallax2D:
+			parallax_list.append(child)
+		
+		find_parallax(child)
+
+func update_preview(new: bool = accurate_preview):
+	accurate_preview = new
+	
+	if not is_inside_tree():
+		return
+	
+	if accurate_preview:
+		if not line_helper:
+			line_helper = Node2D.new()
+			line_helper.z_index = 4096
+			line_helper.draw.connect(draw_outline)
+			get_viewport().add_child(line_helper)
+	else:
+		if line_helper:
+			line_helper.queue_free()
+			line_helper = null
+	
+	last_viewport_pos = Vector2.INF
+
+func _physics_process(delta: float) -> void:
+	var viewport_pos := get_viewport().global_canvas_transform.origin
+	
+	if viewport_pos != last_viewport_pos:
+		last_viewport_pos = viewport_pos
+		
+		var position: Vector2
+		if accurate_preview:
+			position = get_editor_view_rect().position
+		else:
+			position = -viewport_pos / get_viewport().global_canvas_transform.get_scale()
+		
+		for parallax in parallax_list:
+			parallax.screen_offset = position
+		
+		if accurate_preview:
+			line_helper.queue_redraw()
+
+func get_editor_view_rect() -> Rect2:
+	var viewport_scale := get_viewport().global_canvas_transform.get_scale()
+	var viewport_pos := get_viewport().global_canvas_transform.origin / viewport_scale
+	var view_rect := Rect2(-viewport_pos, Vector2(get_viewport().size) / viewport_scale)
+	return Rect2(view_rect.position + view_rect.size * 0.5 - window_size * 0.5, window_size)
+
+func draw_outline() -> void:
+	if Engine.is_editor_hint():
+		var rect := get_editor_view_rect()
+		draw_line(rect.position, rect.position + Vector2(rect.size.x, 0))
+		draw_line(rect.position, rect.position + Vector2(0, rect.size.y))
+		draw_line(rect.position + Vector2(rect.size.x, 0), rect.end)
+		draw_line(rect.position + Vector2(0, rect.size.y), rect.end)
+
+func draw_line(from: Vector2, to: Vector2):
+	line_helper.draw_dashed_line(from, to, Color.YELLOW, -1, 4, false)
+	line_helper.draw_dashed_line(from + from.direction_to(to) * 4, to, Color.BLACK, -1, 4, false)
